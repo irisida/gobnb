ee21cd6fce7fcb826df307ec4d24f3dcb4713a7a
irisida <20445648+irisida@users.noreply.github.com>
2021-06-24 16:47:24 +0100

    updated readme for 3rd party packages with links

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -27,14 +27,14 @@ func NewHandlers(r *Repository) {
 	Repo = r
 }
 
-// Home is the home page handler
+// Home is the home page handler.
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
-// About is the about page handler
+// About is handler function for page to be shown at the /about route.
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello again"
--- readme.md
+++ readme.md
@@ -4,6 +4,6 @@ A boutique hotel/guesthouse/BnB type booking & management project.
 
 ### Construction Overview
 - Built using Go1.16
-- github.com/alexedwards/scs/v2 v2.4.0
-- github.com/go-chi/chi v1.5.4
-- github.com/justinas/nosurf v1.1.1
+- [github.com/alexedwards/scs/v2 v2.4.0](https://github.com/alexedwards/scs)
+- [github.com/go-chi/chi v1.5.4](https://github.com/go-chi/chi)
+- [github.com/justinas/nosurf v1.1.1](https://github.com/justinas/nosurf)
